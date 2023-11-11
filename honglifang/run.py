720cf2a28c07efa074c6001ba78c98b97da6632d
yuzhegan <boflyyang@163.com>
2023-11-11 16:39:18 +0800

    fix proxy & muplite times to request

--- honglifang/run.py
+++ honglifang/run.py
@@ -32,12 +32,12 @@ def run_script(arguments):
 # params5 = ['-u', '18720600577', '-p', 'hlf4210', '-d', '2023-11-04', '-a', '下午场一'] # 两个2小
 # params6 = ['-u', '18974573951', '-p', 'hlf123', '-d', '2023-11-04', '-a', '下午场二']  # 两个2小
 # params7 = ['-u', '18172856581', '-p', 'wq6666', '-d', '2023-11-04', '-a', '晚上']  # 两个2小
-params1 = ['-u', '15707974367', '-p', 'ccc123','-d', '2023-11-11', '-a', '上午']  # 两个一小
-params2 = ['-u', '18870011708', '-p', 'sb123456','-d', '2023-11-11', '-a', '下午场一']  # 两个一小
-params3 = ['-u', '18146700772', '-p', '112580','-d', '2023-11-11', '-a', '下午场一']  # 两个一小
-params4 = ['-u', '18126152622', '-p', '221212','-d', '2023-11-11', '-a', '下午场二']  # 两个2小
-params5 = ['-u', '15907986641', '-p', '451234','-d', '2023-11-11', '-a', '上午']  # 两个2小
-params6 = ['-u', '18466238170', '-p', 'hlf127','-d', '2023-11-11', '-a', '下午场二']  # 两个2小
+params1 = ['-u', '15707974367', '-p', 'ccc123','-d', '2023-11-18', '-a', '上午']  # 两个一小
+params2 = ['-u', '18870011708', '-p', 'sb123456','-d', '2023-11-18', '-a', '下午场一']  # 两个一小
+params3 = ['-u', '18146700772', '-p', '112580','-d', '2023-11-18', '-a', '下午场一']  # 两个一小
+params4 = ['-u', '18126152622', '-p', '221212','-d', '2023-11-18', '-a', '下午场二']  # 两个2小
+params5 = ['-u', '15907986641', '-p', '451234','-d', '2023-11-18', '-a', '上午']  # 两个2小
+params6 = ['-u', '18466238170', '-p', 'hlf127','-d', '2023-11-18', '-a', '下午场二']  # 两个2小
 # params6 = ['-u', '18974573951', '-p', 'hlf123', '-d', '2023-11-04', '-a', '下午场二']  # 两个2小
 # params7 = ['-u', '18172856581', '-p', 'wq6666', '-d', '2023-11-04', '-a', '晚上']  # 两个2小
 # params = [params1, params2]
@@ -64,17 +64,18 @@ processes = []
 # print(proxies)
 
 
-current_time = time.localtime()
-# 设置目标时间为 10:53:00
-target_time = time.struct_time((current_time.tm_year, current_time.tm_mon, current_time.tm_mday,
-                               16, 00, 0, current_time.tm_wday, current_time.tm_yday, current_time.tm_isdst))
-# 计算需要等待的秒数
-current_timestamp = time.mktime(current_time)
-target_timestamp = time.mktime(target_time)
-wait_seconds = target_timestamp - current_timestamp
-print("等待", wait_seconds, "秒")
-# 等待到目标时间
-time.sleep(wait_seconds)
+def GetSleepTimeSecend(hour, minute, second):
+    current_time = time.localtime()
+    # 设置目标时间为 10:53:00
+    target_time = time.struct_time((current_time.tm_year, current_time.tm_mon, current_time.tm_mday,
+
+                                   hour, minute, second, current_time.tm_wday, current_time.tm_yday, current_time.tm_isdst))
+    # 计算需要等待的秒数
+    current_timestamp = time.mktime(current_time)
+    target_timestamp = time.mktime(target_time)
+    wait_seconds = target_timestamp - current_timestamp
+    print("等待", wait_seconds, "秒")
+    return wait_seconds
 def GenJdIps():
     proxy = Proxy_JiGuang()
     proxy.SetLocalIp2WhiteList()
@@ -82,33 +83,47 @@ def GenJdIps():
     ic(ips)
     return ips
 
-try:
-    ips = GenJdIps()
-except Exception as e:
-    print(e)
-    for i in range(3):
-        time.sleep(3)
+# 提前1分钟请求下来代理ip 代理ip可以用1-2分钟
+time.sleep(GetSleepTimeSecend(15, 59, 40))
+for i in range(3):
+    try:
         ips = GenJdIps()
+        for param in params:
+            ip = random.choice(ips)
+            proxies = {
+                "http": "http://" + str(ip['ip'])+':'+str(ip['port']),
+                "https": "https://" + str(ip['ip'])+':'+str(ip['port']),
+            }
+            # proxies = convert_to_dict(str(proxies))
+            param.append('-x')
+            param.append(str(proxies))
+        break
+    except Exception as e:
+        ic(e)
 
-for param in params:
-    ip = random.choice(ips)
-    proxies = {
-        "http": "http://" + str(ip['ip'])+':'+str(ip['port']),
-        "https": "https://" + str(ip['ip'])+':'+str(ip['port']),
-    }
-    # proxies = convert_to_dict(str(proxies))
-    param.append('-x')
-    param.append(str(proxies))
-
-ic(params)
+import datetime
+current_time = datetime.datetime.now().strftime('%H:%M:%S')
+print(current_time)
 
 
-# 创建并启动进程
-for param in params:
-    p = Process(target=run_script, args=(param,))
-    p.start()
-    processes.append(p)
+# 等待到16：00：00 开始执行抢票
+# 申请完代理看下时间有么有到16点
+current_hour = time.localtime().tm_hour
+if current_hour < 16:
+    time.sleep(GetSleepTimeSecend(16, 00, 00))
 
-# 等待所有进程完成
-for p in processes:
-    p.join()
+for i in range(2):
+    try:
+        # 创建并启动进程
+        for param in params:
+            p = Process(target=run_script, args=(param,))
+            p.start()
+            processes.append(p)
+        # 等待所有进程完成
+        for p in processes:
+            p.join()
+    except Exception as e:
+        ic(e)
+
+
+
