753a5759e004c21f61d30a4c4f480451d22ab00e
dav <yuzhegan>
2024-01-02 16:04:18 +0800

    add files

---
+++ ozon/demo2.py
@@ -0,0 +1,79 @@
+# encoding='utf-8
+
+# @Time: 2023-12-30
+# @File: %
+#!/usr/bin/env
+from icecream import ic
+import time
+from curl_cffi import requests
+from lxml import etree
+import os
+import json
+from parseData import ParseData
+from parseData import *
+
+
+class Ozon_Spider:
+    def __init__(self):
+        self.headers = {
+            'authority': 'www.ozon.ru',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        }
+
+    def Get_cookies(self):
+        response = requests.get('https://www.ozon.ru/abt/result',
+                                impersonate="chrome110", headers=self.headers).cookies
+        cookies = response
+        return cookies
+
+    def searchResultsV2(self, cookies, searh_text, page):
+        params = {
+            # 'from_global': 'true',
+            'page': page,
+            'text': searh_text,
+        }
+        response = requests.get('https://www.ozon.ru/search/', params=params,
+                                cookies=cookies, headers=self.headers, impersonate="chrome110")
+        #write to file
+        # with open('ozon.txt', 'w', encoding='utf-8') as f:
+        #     f.write(response.text)
+        html = etree.HTML(response.text)
+        data = html.xpath('//div[contains(@id,"state-searchResultsV2")]/@data-state')
+        data = json.loads(data[0])
+        # with open('ozon.json', 'w', encoding='utf-8') as f:
+        #     f.write(json.dumps(data, indent=4, ensure_ascii=False))
+        return data
+
+
+
+if __name__ == '__main__':
+    search_text = 'мыльная бабочка'
+    ozon = Ozon_Spider()
+    cookies = ozon.Get_cookies()
+    writer = DataWriter('data.csv')
+    str_today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+
+    for page in range(1, 8):
+        # page = 1
+        time.sleep(1)
+        data = ozon.searchResultsV2(cookies, search_text, page)
+        print(len(data['items']))
+        datas = ParseData(data)
+        datas.parseData(writer, search_text, str_today, page)
+    writer.close()
+
+
+    
+
---
+++ tianwentai/demo.py
@@ -0,0 +1,116 @@
+# encoding='utf-8
+
+# @Time: 2024-01-01
+# @File: %
+#!/usr/bin/env
+from icecream import ic
+import os
+import requests
+import re
+
+
+class TianWenTai:
+    def __init__(self):
+        self.headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "Origin": "https://weather.121.com.cn",
+            "Pragma": "no-cache",
+            "Referer": "https://weather.121.com.cn/sztwt/?",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6309071d) XWEB/8447 Flue"
+        }
+        self.session = requests.session()
+        self.session.headers.update(self.headers)
+        self.wxuser = 'dadc8a99aac94f1692e01a08a68d1b17'
+        self.token, self.tokenId = self.get_token()
+        self.week = '星期五'
+        self.pmAm = "上午"
+
+    def get_token(self):
+        url = "https://weather.121.com.cn/szqx/api/token.js"
+        response = self.session.get(url).text
+        token = re.search('win\.szqbl\.token="([^"]+)"', response).group(1)
+        print(token)
+        tokenId = re.search('win\.szqbl\.tokenId="([^"]+)"', response).group(1)
+        print(tokenId)
+        return token, tokenId
+
+    def get_tickets_info(self):
+        url = "https://weather.121.com.cn/szqx/api/twt_v2/kfr/list.do"
+        params = {
+            "source": "wx",
+            "wxuser": self.wxuser,
+            "token": self.token,
+            "tokenId": self.tokenId
+        }
+        data = {
+            "yzm": ""
+        }
+        response = self.session.post(url, params=params, data=data)
+        print(response.text)
+        # if response["code"] == 0:
+        #     return response["data"]
+        # else:
+        #     return None
+        return
+
+    def Parse_data(self, data, week):
+        # 判断是否有票
+        get_ticket_key = data['dydptx']
+        ticket_key_lists = []
+        for key in get_ticket_key:
+            ticket_key_lists.append(key.keys())
+        # 获取星期几和日期信息所对于的key
+        for item in ticket_key_lists:
+            if (week in item):
+                ture_key = item
+        # 判断是否有余票以及有几张
+        ticket_nums = data["kfrs"][ture_key]   # 得到一个上午和下午的列表
+        for item in ticket_nums:
+            if self.pmAm in item['name']:
+                available_ticket = int(item['maxNum']) - int(item['curNum'])
+        print(week, "有", available_ticket, "张票")
+        return available_ticket
+
+
+if __name__ == "__main__":
+    twt = TianWenTai()
+    data = twt.get_tickets_info()
+    # twt.Parse_data(data, twt.week)
+
+
+"""
+import requests
+
+
+params = {
+    "source": "wx",
+    "wxuser": "8bdeff9194094319a6854594b8051726",
+    "token": "639957149F191A16379C27F5C4169541F6EDB2B29C82940A5EAEC8B90B58E0DC7AA454C9ED7682CFBE945E124F7762CA6EEC615CFB1B9E603B8A46103E45E71F752CE59C522A0D423C0DA31CAFDAD402729A05E8A9036B5FBC7C3B408EE98F55E327887F800ACFD2E3E8A104BF31478E50A4C5DD859265CEF01A5C3C05D2E7C73393597A913F42382A7310F10EA86CBD30FE0EA4072D99DE85EED531A152A7E6576C0AF5E3189B5F986DDFE292A4733CA6EDE5C3B13B2025526AD82089DABAF4CB7C55251545B7D75E6FF44DCF6544C42855311992D151E4C2B1D55F6CECED5110E6F27DE71AE91DE64B5C296587F9D1C29E080764DB85B6F1821A0EF3670C4ECDD44D3082802387EEDFE038770358ABAA447CEAC9F7EA4206579AEB4DBEAB9DA397EDD1A1FC9A2D3F2A9383F7FA83682AD06499D4C93A5FE9D41A928831736D2E3C80703A8626CFBAFE40F7DAD0CA4B94F025FF39C923D4EAAAD47C919A1EF6BFF4B515F96BEEE0BF578CAD021D3BF5A495B48B07CC3A880D88DA5EA1E6D2A9",
+    "tokenId": "1656753922-1290373290-1689564990-1889704473"
+}
+data = {
+    "dyEndDate": "",
+    "isTYXY": "no",
+    "name": "牛柏寒",
+    "cardType": "1",
+    "cardNumber": "411024199308248600",
+    "sex": "0",
+    "mobile": "18566660591",
+    "carNumber": "",
+    "entourageCount": "0",
+    "nonage": "0",
+    "kfrSdId": "100220",
+    "kfrId": "201764",
+    "isDydptx": "0"
+}
+response = requests.post(url, headers=headers, cookies=cookies, params=params, data=data)
+
+print(response.text)
+print(response)"""
